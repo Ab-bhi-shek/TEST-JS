a0a308bfcad90e6415382805fad3e76002c6112a
Ab-bhi-shek <mrsseema541@gmail.com>
2024-11-02 12:55:10 +0000

    learn var const and let

---
+++ Basics_01/01_variable.js
@@ -0,0 +1,11 @@
+const accountId ="28593"
+let accountName = "Abhishek"
+var accountEmail ="abhishek.google.com"
+accountCity="Hapur"
+accountState=""
+// accountId = "2"
+// accountName ="Ashish sharma"
+// accountEmail ="ashish.google.com"
+// accountCity="Delhi"
+
+console.log([accountId,accountEmail,accountName,accountCity,accountState]);
