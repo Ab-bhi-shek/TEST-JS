7d5d7bc7f9cba3efd19d34dd038324e4a8376df9
Ab-bhi-shek <mrsseema541@gmail.com>
2024-11-04 06:21:06 +0000

    learn conversion

--- Basics_01/02_datatypes.js
+++ Basics_01/02_datatypes.js
@@ -1,8 +1,8 @@
  "use strict"; // treat all js code as newer version
  //alert (3+3) // we use node js not browser
  let name = "Abhishek"
- let age = "19"
- let city = "delhi"
- let state = 
- console.log(typeof "19");
+ let age = 19
+ let city = "delhi" 
+ let state = null
+ console.log(typeof state);
  
---
+++ Basics_01/03_conversion.js
@@ -0,0 +1,17 @@
+let accountLogedIn = "Hitesh"
+let BooleanLogedIn = Boolean (accountLogedIn)
+console.log( BooleanLogedIn);
+console.log( typeof BooleanLogedIn);
+
+let number = 93
+let stringNumber = String (number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let name = "Abhishek"
+let nameNumber = Number (name)
+console.log(nameNumber);
+console.log(typeof nameNumber);
+
+
+
